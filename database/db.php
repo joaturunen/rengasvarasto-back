9cff5b4a7865a929f8fa7d5af67076d07d631f35
Hannes Sirviö <c9siha01@students.oamk.fi>
2021-11-23 11:50:28 +0200

    Database index fixes

--- database/db.php
+++ database/db.php
@@ -6,18 +6,18 @@ create database tirehotel
 with
 owner = postgres
 ENCODING = 'UTF8'
-lc_collate= 'Finnish_Finland.1252'
-lc_ctype = 'Finnish_Finland.1252'
+lc_collate= 'English_Finland.1252'
+lc_ctype = 'English_Finland.1252'
 tablespace = pg_default
 connection limit = -1;
 
 create table role (
-id bigserial primary key,
+id smallserial primary key,
 title varchar(25)
 );
 
 create table employee (
-id bigserial primary key,
+id smallserial primary key,
 firstname varchar(25) not null,
 lastname varchar(25) not null,
 phone varchar(25) not null unique,
@@ -32,12 +32,12 @@ foreign key (role_id) references role(id)
 on delete restrict
 );
 
-create index on employee (
+create index role_index on employee (
 role_id
 );
 
 create table customer (
-id bigserial primary key,
+id smallserial primary key,
 firstname varchar(25) not null,
 lastname varchar(25) not null,
 phone varchar(25) not null unique,
@@ -46,17 +46,17 @@ address varchar(50),
 zipcode char(5),
 city varchar(25),
 customersaved timestamp default current_timestamp,
-employee_id int not null,
+employee_id int not null,                          
 foreign key (employee_id) references employee(id)
 on delete restrict
 );
 
-create index on customer (
+create index employee_index on customer (
 employee_id
 );
 
 create table orders (
-id bigserial primary key,
+id smallserial primary key,
 orderdate timestamp default current_timestamp,
 customer_id int not null,
 employee_id int not null,
@@ -65,44 +65,44 @@ foreign key (employee_id) references employee(id)
 on delete restrict
 );
 
-create index on orders (
+create index orders_index on orders (
 customer_id, employee_id
 );
 
 create table services (
-id bigserial primary key,
+id smallserial primary key,
 service varchar(50),
 price int
 );
 
 create table ordertable (
-orders_id int primary key,
+orders_id int primary key, 
 services_id int not null,
 foreign key (orders_id) references orders(id),
 foreign key (services_id) references services(id)
 on delete restrict
 );
 
-create index on ordertable (
-orders_id, services_id
+create index ordertable_index on ordertable (
+orders_id, services_id 
 );
 
 create table car (
-id bigserial primary key,
-register varchar(25) not null unique,
-brand varchar(25) not null,
+register varchar(25) primary key,
+brand varchar(25) not null,     
 model varchar(25),
+year char(4),
 customer_id int not null,
 foreign key (customer_id) references customer(id)
 on delete restrict
 );
 
-create index on car (
+create index car_index on car (
 customer_id
 );
 
 create table office (
-id bigserial primary key,
+id smallserial primary key,
 name varchar(25) not null unique,
 phone varchar(25) not null unique,
 email varchar(25) not null unique,
@@ -113,43 +113,42 @@ logo varchar(25)
 );
 
 create table warehouse (
-id bigserial primary key,
+id smallserial primary key,
 name varchar(25),
 office_id int not null,
 foreign key (office_id) references office(id)
 on delete restrict
 );
 
-create index on warehouse (
+create index warehouse_index on warehouse (
 office_id
 );
 
 create table shelf (
-id int primary key,
+id smallint primary key, 
 warehouse_id int not null,
 foreign key (warehouse_id) references warehouse(id)
 on delete restrict
 );
 
-create index on shelf (
+create index shelf_index on shelf (
 warehouse_id
 );
 
 create table slot (
-id int primary key,
-shelf_id int not null,
+id smallint primary key,
+shelf_id smallint not null,
 foreign key (shelf_id) references shelf(id)
 on delete restrict
 );
 
-create index on slot (
+create index slot_index on slot (
 shelf_id
 );
 
 create table tires (
-id bigserial primary key,
-car_id int not null,
-slot_id int not null,
+car_register varchar(25) primary key,
+slot_id smallint not null,
 brand varchar(25),
 model varchar(50),
 type varchar(25),
@@ -164,14 +163,11 @@ text text,
 rims varchar(25),
 servicedate timestamp default current_timestamp,
 info text,
-foreign key (car_id) references car(id),
+foreign key (car_register) references car(register),            
 foreign key (slot_id) references slot(id)
-on delete restrict,
-employee_id int not null,
-foreign key (employee_id) references employee(id)
 on delete restrict
 );
 
-create index on tires (
-car_id, slot_id, employee_id
+create index tires_index on tires (
+car_register, slot_id
 );
