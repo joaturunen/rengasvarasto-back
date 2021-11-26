8dd0c895572125ba082a83420556455f93f9cbe9
Joanna Turunen <n0tujo00@students.oamk.fi>
2021-11-26 14:23:04 +0200

    search

---
+++ API/customer/customer_search.php
@@ -0,0 +1,34 @@
+<?php
+require_once '../../inc/headers.php';
+require_once '../../inc/functions.php';
+
+//$db = null;
+
+$input = json_decode(file_get_contents('php://input'));
+$searchCriteria = filter_var($input->searchCriteria, FILTER_SANITIZE_STRING);
+
+// $url = parse_url(filter_input(INPUT_SERVER,'PATH_INFO'),PHP_URL_PATH);
+// $parameters = explode('/',$url);
+// $searchCriteria = $parameters[1];
+
+try {
+    $db = openDb();
+
+    $sql = "SELECT * FROM customer WHERE phone = :searchCriteria";
+
+    $search = $db->prepare($sql);
+
+    $search->bindValue(":searchCriteria", $searchCriteria, PDO::PARAM_STR);
+
+    $search->execute();
+    $data = $search->fetch(PDO::FETCH_ASSOC);
+  
+    header('HTTP/1.1 200 OK');
+  
+    echo json_encode($data);
+
+    //selectAsJson($db, $sql);
+
+} catch (PDOException $pdoex) {
+    returnError($pdoex);
+}
