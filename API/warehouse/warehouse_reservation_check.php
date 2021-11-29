df780de915cabddf7acd63230f7a86b7d1afeadb
Luomanmäki Alisa <58733101+Sisarus@users.noreply.github.com>
2021-11-29 09:33:39 +0200

    Vapaiden paikkojen tarkastus toimii

---
+++ API/warehouse/warehouse_reservation_check.php
@@ -0,0 +1,25 @@
+<?php
+require_once '../../inc/headers.php';
+require_once '../../inc/functions.php';
+
+$db = null;
+
+try {
+  $db = openDb();
+  $show = $db->prepare("SELECT * FROM slot_order WHERE tires_id IS NULL");
+
+  $show->execute();
+  $data = $show->fetchAll(PDO::FETCH_ASSOC);
+
+  if (count($data) > 0) {
+    $data = array("reservation_free" => true);
+  } else {
+    $data = array("reservation_free" => false);
+  }
+
+  header('HTTP/1.1 200 OK');
+
+  echo json_encode($data);
+} catch (PDOException $pdoex) {
+  returnError($pdoex);
+}
