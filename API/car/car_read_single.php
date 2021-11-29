81a8c77d63af9aa1df58e1559155fa3fbbbf7d25
Joanna Turunen <n0tujo00@students.oamk.fi>
2021-11-29 18:04:33 +0200

    välitallennus

--- API/car/car_read_single.php
+++ API/car/car_read_single.php
@@ -2,12 +2,16 @@
 require_once '../../inc/headers.php';
 require_once '../../inc/functions.php';
 
+// HAKEE ASIAKASNUMEROLLA !!
+
 $db = null;
 
 // Get raw posted data
-// $input = json_decode(file_get_contents('php://input'));
+$input = json_decode(file_get_contents('php://input'));
 
 // $id = filter_var($input->id, FILTER_SANITIZE_NUMBER_INT);
+
+$customer_id = filter_var($input->customer_id, FILTER_SANITIZE_NUMBER_INT);
 
 $id = isset($_GET['id']) ? $_GET['id'] : die();
 
@@ -15,9 +19,9 @@ $id = isset($_GET['id']) ? $_GET['id'] : die();
 
 try {
   $db = openDb();
-  $show = $db->prepare("SELECT * FROM car WHERE id = :id");
+  $show = $db->prepare("SELECT * FROM car WHERE customer_id = :customer_id");
 
-  $show->bindValue(":id", $id, PDO::PARAM_INT);
+  $show->bindValue(":customer_id", $id, PDO::PARAM_INT);
 
   $show->execute();
   $data = $show->fetch(PDO::FETCH_ASSOC);
--- API/customer/customer_search.php
+++ API/customer/customer_search.php
@@ -7,9 +7,6 @@ require_once '../../inc/functions.php';
 $input = json_decode(file_get_contents('php://input'));
 $searchCriteria = filter_var($input->searchCriteria, FILTER_SANITIZE_STRING);
 
-// $url = parse_url(filter_input(INPUT_SERVER,'PATH_INFO'),PHP_URL_PATH);
-// $parameters = explode('/',$url);
-// $searchCriteria = $parameters[1];
 
 try {
     $db = openDb();
@@ -27,7 +24,6 @@ try {
   
     echo json_encode($data);
 
-    //selectAsJson($db, $sql);
 
 } catch (PDOException $pdoex) {
     returnError($pdoex);
