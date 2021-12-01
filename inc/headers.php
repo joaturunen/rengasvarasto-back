14fe6d5fbde8b174cb59ee108d1f0f22e6114b99
Joanna Turunen <n0tujo00@students.oamk.fi>
2021-12-01 18:22:35 +0200

    headers

--- inc/headers.php
+++ inc/headers.php
@@ -2,7 +2,7 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Accept, Content-Type, Access-Control-Allow-Header');
+header('Access-Control-Allow-Headers: Accept, Content-Type, Access-Control-Allow-Header, Origin, X-Auth-Token');
 header('Content-Type: application/json');
 header('Access-Control-Max-Age: 3600');
 
