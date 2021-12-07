7b69dd412277673d9fd15e0f15384f981306c5b0
Joanna Turunen <n0tujo00@students.oamk.fi>
2021-12-07 16:09:27 +0200

    orderia

---
+++ API/order/order_create.php
@@ -0,0 +1,38 @@
+<?php
+require_once '../../inc/headers.php';
+require_once '../../inc/functions.php';
+
+$db = null;
+
+$input = json_decode(file_get_contents('php://input'));
+
+$cus_id = filter_var($input->cus_id, FILTER_SANITIZE_NUMBER_INT);
+$employee_id = filter_var($input->employee_id, FILTER_SANITIZE_NUMBER_INT);
+$cart = $input->cart;
+
+try {
+    $db = openDb();
+    $db->beginTransaction();
+
+    $sql = "INSERT INTO orders (cus_id) 
+        VALUES ('". 
+            filter_var($cus_id,FILTER_SANITIZE_NUMBER_INT) . "','" .
+            filter_var($employee_id,FILTER_SANITIZE_NUMBER_INT) ."')";
+    $order_id = executeInsert($db,$sql);
+
+    foreach ($cart as $services) {
+        $sql = "INSERT INTO ordertable (order_id, service_id)
+            VALUES (" . $order_id . "," . $services->id .")";
+        executeInsert($db, $sql);
+    }
+
+    $db->commit();
+
+    header('HTTP/1.1 200 OK');
+    $data = array('id' => $astunnus);
+    echo json_encode($data);
+}
+catch (PDOException $pdoex) {
+    $db->rollback();
+    returnError($pdoex);
+}
