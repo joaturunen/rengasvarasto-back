2743609fbb4a53b3d9b53d8e60a0d6c6cd2124a3
Joanna Turunen <n0tujo00@students.oamk.fi>
2021-12-09 15:40:18 +0200

    car jutskia

--- API/car/car_create.php
+++ API/car/car_create.php
@@ -3,7 +3,7 @@ require_once '../../inc/headers.php';
 require_once '../../inc/functions.php';
 
 // Get raw posted data
-$input = json_decode(file_get_contents("php://input"));
+$input = json_decode(file_get_contents('php://input'));
 
 $register = filter_var($input->register, FILTER_SANITIZE_STRING);
 $brand = filter_var($input->brand, FILTER_SANITIZE_STRING);
@@ -14,14 +14,13 @@ try {
   //instantiate DB & connect
   $db = openDb();
 
-  // Create query
-  $sql = "INSERT INTO car (register, brand, model, customer_id)
-  VALUES ($register, $brand, $model, $customer_id)";
+  $sql = "INSERT INTO car (register, brand, model, customer_id) 
+    VALUES ('$register', '$brand', '$model', $customer_id)";
 
   $car_id = executeInsert($db, $sql);
 
   header('HTTP/1.1 200 OK');
-  $data = array('id' => $car_id);
+  $data = array('id' => $car_id, 'register' => $register, 'brand' => $brand, 'model' => $model, 'customer_id' => $customer_id);
   echo json_encode($data);
 } catch (PDOException $pdoex) {
   returnError($pdoex);
